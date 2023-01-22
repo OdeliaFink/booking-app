2a845d60358d27bfbc2557f175364c05e4185647
OdeliaFink <odelia.finkel@gmail.com>
2023-01-21 21:32:58 -0500

    Refactor styled components and mapping over icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,61 @@ import { Calendar, DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import format from 'date-fns/format';
+import styled from 'styled-components';
+
+const icons = [
+  { type: faBed, id: 'Stays' },
+  { type: faPlane, id: 'Flights' },
+  { type: faCar, id: 'Car Rentals' },
+  { type: faTaxi, id: 'Airport Taxis' },
+];
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const HeaderDiv = styled.div`
+  background-color: #003580;
+  color: white;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+const HeaderDivContainer = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  margin: 20px 0px 100px 0px;
+`;
+const HeaderList = styled.div`
+  display: flex;
+  gap: 40px;
+  margin-bottom: 50px;
+`;
+const HeaderListItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  &:hover{
+    border: 1px solid white;
+    padding: 10px;
+    border-radius: 20px;
+    cursor: pointer;
+   }
+  }
+`;
+const HeaderButton = styled.div`
+  background-color: #0071c2;
+  color: white;
+  padding: 15px;
+  font-size: 13px;
+  font-weight: 500;
+  border: 1px solid white;
+  cursor: pointer;
+  width: 20%;
+`;
 
 const Header = () => {
   const [openDate, setOpenDate] = useState(false);
@@ -31,31 +86,25 @@ const Header = () => {
     room: 1,
   });
   return (
-    <div className="header">
-      <div className="header-container">
-        <div className="header-list">
-          <div className="header-list-item active">
-            <FontAwesomeIcon icon={faBed} />
-            <span>Stays</span>
-          </div>
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faPlane} />
-            <span>Flights</span>
-          </div>
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faCar} />
-            <span>Car Rentals</span>
-          </div>
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faTaxi} />
-            <span>Taxis</span>
-          </div>
-        </div>
-        <h1 className="header-title">Find your next stay</h1>
+    <HeaderDiv>
+      <HeaderDivContainer>
+        <HeaderList>
+          {icons.map((icon) => {
+            return (
+              <HeaderListItem>
+                <FontAwesomeIcon icon={icon.type} />
+                <span>{icon.id}</span>
+              </HeaderListItem>
+            );
+          })}
+        </HeaderList>
+        <Title className="header-title">Find your next stay</Title>
         <p className="header-description">
           Search deals on hotels, homes, and much more...
         </p>
-        <button className="header-button"> Sign in / Register</button>
+        <HeaderButton className="header-button">
+          Sign in / Register
+        </HeaderButton>
         <div className="header-search">
           <div className="header-search-item">
             <FontAwesomeIcon icon={faBed} className="header-icon" />
@@ -94,8 +143,8 @@ const Header = () => {
             <button className="header-button">Search</button>
           </div>
         </div>
-      </div>
-    </div>
+      </HeaderDivContainer>
+    </HeaderDiv>
   );
 };
 
